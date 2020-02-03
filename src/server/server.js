132165609050593952f0f0b28a681d8483bf5895
Cory R. Siebler <cory.siebler@nextiva.com>
2020-02-03 12:32:47 -0700

    Made server export schema JSON

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,12 @@
+import fs from "fs";
 import express from "express";
 import cors from "cors";
 import GraphQLHTTP from "express-graphql";
 import { MongoClient } from "mongodb";
+import { graphql } from "graphql";
+import { introspectionQuery } from "graphql/utilities";
 
-import Schema from "./data/schema";
+import Schema from "./schema";
 
 const port = 8080;
 const uri =
@@ -15,15 +18,27 @@ app.set("port", process.env.PORT || port);
 app.use(cors());
 
 (async () => {
-  let database = await MongoClient.connect(uri);
+  let database = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  let schema = Schema(database.db("rgrjs"));
 
   app.use(
     "/graphql",
     GraphQLHTTP({
-      schema: Schema(database.db("rgrjs")),
+      schema: schema,
       graphiql: true
     })
   );
 
   app.listen(port, () => console.log(`Listening on port ${port}`));
+
+  // Generate schema.json
+  let json = await graphql(schema, introspectionQuery);
+  fs.writeFile("./data/schema.json", JSON.stringify(json, null, 2), err => {
+    if (err) throw err;
+
+    console.log("JSON schema created");
+  });
 })();
